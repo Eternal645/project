5004d2a45d87dc30433a8af156d098d29ad0b819
Victor Shulgin <shulgins1978@gmail.com>
2026-06-05 19:27:03 +0800

    add core folder with __init__.py to packages

---
+++ packages/core/__init__.py
@@ -0,0 +1,8 @@
+from .order_logic import OrderLine, calculate_order_total, count_product_in_lines, validate_order_lines
+
+__all__ = [
+    "OrderLine",
+    "calculate_order_total",
+    "count_product_in_lines",
+    "validate_order_lines",
+]
